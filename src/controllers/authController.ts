5a4690f0a70c1056bc456fd6d69604472d4a5521
pxdrobdf <phladev05@gmail.com>
2024-07-20 01:46:15 -0300

    feat: add login route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express'
-import { userSchema } from '../schemas/userSchema'
-import bcrypt from 'bcryptjs'
+import { loginUserSchema, registerUserSchema } from '../schemas/userSchema'
 import { db } from '../lib/prisma'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
-    const parsed = userSchema.safeParse(req.body)
+    const parsed = registerUserSchema.safeParse(req.body)
 
     if (!parsed.success) {
       return res.status(400).json({ errors: parsed.error.errors[0].message })
@@ -17,6 +18,29 @@ export const registerUser = async (req: Request, res: Response) => {
 
     const user = await db.user.create({data: {name, email, password: hashedPassword}})
     res.status(201).json(user)
+  } catch (error) {
+    if(error instanceof Error)
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const parsed = loginUserSchema.safeParse(req.body)
+
+    if (!parsed.success) {
+      return res.status(400).json({ errors: parsed.error.errors[0].message })
+    }
+
+    const { email, password } = parsed.data
+
+    const user = await db.user.findUnique({where: {email}})
+
+    if(!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid email or password' })
+    }
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET as string, { expiresIn: '1h' })
+    res.status(200).json({ token })
   } catch (error) {
     if(error instanceof Error)
     res.status(500).json({ message: error.message })
--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 
-export const userSchema = z.object({
+export const registerUserSchema = z.object({
   name: z.string().min(1, 'Name is required!'),
+  email: z.string().email('Please enter a valid email!'),
+  password: z.string().min(6, 'Please enter a valid password!')
+})
+
+export const loginUserSchema = z.object({
   email: z.string().email('Please enter a valid email!'),
   password: z.string().min(6, 'Please enter a valid password!')
 })
