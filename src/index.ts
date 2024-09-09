05a76fb984b04d3f65f7a14f33454b84efc0f8a8
pxdrobdf <phladev05@gmail.com>
2024-09-09 19:07:14 -0300

    fix: fix the admin routes file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes'
+import adminRoutes from './routes/adminRoutes'
 
 dotenv.config()
 
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/api', userRoutes)
-app.use('/api/admin', userRoutes)
+app.use('/api/admin', adminRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
