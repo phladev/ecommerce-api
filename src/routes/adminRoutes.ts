4b922b5944ef71c2ecc378c233906b19cb823300
pxdrobdf <phladev05@gmail.com>
2024-09-10 15:19:51 -0300

    refactor: remove unnecessary get route for products

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { loginAdmin, registerAdmin } from '../controllers/authController'
-import { deleteProduct, getProducts, registerProduct, updateProduct } from '../controllers/productsController'
+import { deleteProduct, registerProduct, updateProduct } from '../controllers/productsController'
 import { authMiddlewareADM } from '../middlewares/authMiddleware'
 import { getOrders } from '../controllers/orderController'
 
@@ -12,7 +12,6 @@ router.post('/register', registerAdmin)
 router.post('/login', loginAdmin)
 
 //Product
-router.get('/products', getProducts)
 router.post('/products', authMiddlewareADM, registerProduct)
 router.put('/products', authMiddlewareADM, updateProduct)
 router.delete('/products/:id', authMiddlewareADM, deleteProduct)
